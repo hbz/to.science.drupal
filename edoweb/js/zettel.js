1aa1aa28aa1642792b6fc206d0be7a3d7f0c4e7b
Andres Quast <quast@hbz-nrw.de>
2025-01-20 10:44:48 +0100

    work in progress

--- edoweb/js/zettel.js
+++ edoweb/js/zettel.js
@@ -24,24 +24,20 @@
 			if (!isEmpty($('.tabs', context))) {
 				loadZettel(bundle, entity, context);
 			} else {
-				if (bundle != 'ktblData'){
+				if (bundle == 'ktblData'){
 					var zettel_form = '<h1>HUHU this is ktbl</h1><div id="successBox" class="success"></div>'
 							+ '<div id="warningBox" class="warning"></div>'
 							+ '<div><a href=">' + Drupal.settings.edoweb.formsServiceUrl + '/ktblData" target="_blank">'
 							+ Drupal.settings.edoweb.formsServiceUrl + '</a></div>'
-							+ '<iframe class="'+bundle+'"name="'+Date.now()+'" src="'
+							/* + '<iframe class="'+bundle+'"name="'+Date.now()+'" src="'
 							+ Drupal.settings.edoweb.formsServiceUrl
-							+ '/forms'
-							+ '?id=katalog:'
-							+ bundle
-							+ '&format=xml'
-							+ '&documentId=_:foo'
+							+ '/forms/researchdataktbl'
 							+ '&topicId='
 							+ Drupal.settings.baseUrl
 							+ '/resource/add/'
 							+ bundle
 							+ '"'
-							+ ' width="800px" height="1024px" style="border: none;position:relative;" id="iFrame">'
+							+ ' width="800px" height="1024px" style="border: none;position:relative;" id="iFrame">' */
 							+ '<p>iframes are not supported by your browser.</p></iframe>';		
 						$('.region.region-content').html(zettel_form);
 				} else { 
