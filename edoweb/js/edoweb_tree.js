012e730f87657f571be6d45bbc3703beca320c7a
hadoud <hasan.adoud@hbz-nrw.de>
2026-07-20 18:34:24 +0200

    Hide Neue Überordnung(part) and Neue Publikation(article) in View/Ansicht-tab

--- edoweb/js/edoweb_tree.js
+++ edoweb/js/edoweb_tree.js
@@ -55,6 +55,8 @@
 
       if (Drupal.settings.actionAccess) {
         findTargetBundles($('.edoweb-tree>li>a', context).attr('data-bundle'), function(bundle) {
+		// According to Lara(ZBMED), Show only file(Neue Datei) in tree add menu
+		if(bundle!='file') return ;
           var link = $('<a />')
             .attr('href', Drupal.settings.basePath + 'resource/add/' + target_bundle)
             .attr('data-bundle', target_bundle)
